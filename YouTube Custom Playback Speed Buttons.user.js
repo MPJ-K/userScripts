dde4e0c53c0fc8e642ede264dad99785c83ca377
MPJ / Quadryo <104721042+MPJ-K@users.noreply.github.com>
2023-01-02 23:52:44 +0100

    Update version date

--- YouTube Custom Playback Speed Buttons.user.js
+++ YouTube Custom Playback Speed Buttons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Custom Playback Speed Buttons
 // @namespace    MPJ_namespace
-// @version      27-11-2022
+// @version      02-01-2023
 // @description  Adds easily accessible playback speed buttons for selectable speeds up to 10x and an option to remember the speed. More features can be found in the script settings.
 // @author       MPJ
 // @match        https://*.youtube.com/*
