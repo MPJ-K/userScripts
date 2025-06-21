07fe275b07b543f6430ee42867b4ec1daa82c2db
MPJ-K <maurice.kneepkens@outlook.com>
2025-06-21 23:42:02 +0200

    Fix #63: Updated for better responsiveness and consistency.

--- Redirect_YouTube_Shorts.user.js
+++ Redirect_YouTube_Shorts.user.js
@@ -1,26 +1,36 @@
 // ==UserScript==
 // @name         Redirect YouTube Shorts
 // @namespace    MPJ_namespace
-// @version      2023.09.08.01
-// @description  Redirects any YouTube Shorts video to a watch page with the normal player
+// @version      2025.06.21.01
+// @description  Automatically redirects all YouTube Shorts videos to a regular 'watch' page.
 // @author       MPJ
 // @match        https://www.youtube.com/*
+// @exclude      https://www.youtube.com/live_chat*
+// @run-at       document-start
 // @icon         https://www.youtube.com/favicon.ico
 // @grant        none
-// @updateURL    https://github.com/MPJ-K/userScripts/raw/main/Redirect%20YouTube%20Shorts.user.js
-// @downloadURL  https://github.com/MPJ-K/userScripts/raw/main/Redirect%20YouTube%20Shorts.user.js
+// @updateURL    https://github.com/MPJ-K/userScripts/raw/main/Redirect_YouTube_Shorts.user.js
+// @downloadURL  https://github.com/MPJ-K/userScripts/raw/main/Redirect_YouTube_Shorts.user.js
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    // Add an event listener that runs attemptRedirect() whenever a new YouTube page loads.
-    document.addEventListener("yt-navigate-finish", attemptRedirect);
-
-    // The main function that detects Shorts pages and redirects them to a watch page.
-    function attemptRedirect() {
+    /**
+     * Redirect all YouTube Shorts videos to a regular 'watch' page.
+     */
+    function redirectShortsToWatch() {
         const URL = document.URL;
-        const shortsID = "/shorts/";
-        if (URL.includes(shortsID)) { window.location = URL.replace(shortsID, "/watch?v="); }
+        const shortsIdentifier = "/shorts/";
+        if (URL.includes(shortsIdentifier)) {
+            window.location = URL.replace(shortsIdentifier, "/watch?v=");
+        }
     }
+
+
+    // Add an event listener that runs redirectShortsToWatch() whenever a new YouTube page loads.
+    document.addEventListener("yt-navigate-start", redirectShortsToWatch);
+
+    // When the script starts, call redirectShortsToWatch() directly to improve responsiveness.
+    redirectShortsToWatch();
 })();
